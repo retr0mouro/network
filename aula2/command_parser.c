9a20603b77207a028953bc5d882bd4dbcbdeafec
retr0mouro <retr0.mouro@gmail.com>
2023-09-26 13:42:06 +0100

    inicio da command_parsing

--- aula2/command_parser.c
+++ aula2/command_parser.c
@@ -1,12 +1,12 @@
 #include "command_parser.h"
 
-/*  
+/*
     testes:
         11/11/2023
         15/2/2024
 
-    quando crasha : 
-    
+    quando crasha :
+
     1    ulimit -c unlimited
 
     2    gcc -Wall -g
@@ -16,9 +16,48 @@
 */
 
 void processCommand(Deque* deque, Cmd* cmd){
-    
+	if(cmd->command == "PUSH"){
+		for(int i = 0;i < cmd->nargs;push(deque,args[i]),i++);
+		return;
+	}
+	if(cmd->command == "PUSH_FRONT"){
+		for(int i = 0;i < cmd->nargs;pushFront(deque,args[i]),i++);
+		return;
+	}
+	if(cmd->command == "POP"){
+		if(!deque){
+			printf("EMPTY");
+			return;
+		}
+		printFunc(pop(deque));
+		return;
+ 	}
+ 	if(cmd->command = "POP_FRONT"){
+ 		if(!deque){
+ 			printf("EMPTY");
+ 			return;
+ 		}
+ 		printFunc(popFront(deque));
+ 		return;
+ 	}
+ 	if(cmd->command == "SIZE"){
+ 		printf("%d",size(deque));
+ 		return;
+ 	}
+ 	if(cmd->command == "REVERSE"){
+ 		reverse(deque);
+ 		return;
+ 	}
+ 	if(cmd->command == "PRINT"){
+ 		printDeque(deque,printFunc);
+ 		return;
+ 	}
 }
 
 Cmd* parseLine(char* line){
-    
+    if(!line) return NULL;
+    Cmd* comando = malloc(sizeof(struct cmd));
+    char* safe = strcpy(safe,line);
+    char* cmd = strsep(&line," ");
+
 }
--- aula2/command_parser.h
+++ aula2/command_parser.h
@@ -12,44 +12,7 @@ typedef struct cmd {
     int nargs; //número de argumentos
 } Cmd;
 
-void processCommand(Deque* deque, Cmd* cmd){
-	if(cmd->command == "PUSH"){
-		for(int i = 0;i < cmd->nargs;push(deque,args[i]),i++);
-		return;
-	}
-	if(cmd->command == "PUSH_FRONT"){
-		for(int i = 0;i < cmd->nargs;pushFront(deque,args[i]),i++);
-		return;
-	}
-	if(cmd->command == "POP"){
-		if(!deque){
-			printf("EMPTY");
-			return;
-		}
-		printFunc(pop(deque));
-		return;
- 	} 
- 	if(cmd->command = "POP_FRONT"){
- 		if(!deque){
- 			printf("EMPTY");
- 			return;
- 		}
- 		printFunc(popFront(deque));
- 		return;
- 	}
- 	if(cmd->command == "SIZE"){
- 		printf("%d",size(deque));
- 		return;
- 	}
- 	if(cmd->command == "REVERSE"){
- 		reverse(deque);
- 		return;
- 	}
- 	if(cmd->command == "PRINT"){
- 		printDeque(deque,printFunc);
- 		return;
- 	}
-}
+void processCommand(Deque* deque, Cmd* cmd);
 
 Cmd* parseLine(char* line);
 
