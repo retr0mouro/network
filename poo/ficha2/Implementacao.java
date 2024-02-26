f750234598f30af3cb8bd603aecbc94e8cf6dd51
a104360 <a104360@alunos.uminho.pt>
2024-02-26 08:53:24 +0000

    merge sort implementado

--- poo/ficha2/Implementacao.java
+++ poo/ficha2/Implementacao.java
@@ -1,4 +1,5 @@
 package pacote;
+import java.util.Arrays;
 
 public class Implementacao{
     public int findMin(int[] array){
@@ -9,5 +10,112 @@ public class Implementacao{
             }
         }
         return min;
+    }
+    public int[] window(int[] array,int a, int b){
+        int size = b - a + 1;
+        int[] window = new int[size];
+        for(int i = 0;i < size;i++){
+            window[i] = array[i + a];
+        }
+        return window;
+    }
+    public void common(int[] a,int[] b){
+        mergeSort(a,a.length);
+        printArray(a,a.length);
+        mergeSort(b,b.length);
+        printArray(b,b.length);
+        
+    }
+    public void mergeSort(int[] array,int length){
+        mSortRecursion(array,0,length - 1);
+    }
+    private void mSortRecursion(int[] array,int l, int r){
+        if(l < r){
+            int m = l + (r - l) / 2;
+            mSortRecursion(array,l,m);
+            mSortRecursion(array,m + 1,r);
+
+            mergeSortedArrays(array,l,m,r);
+        }
+    }
+    private void mergeSortedArrays(int[] array,int l, int m, int r){
+        // Get length of the right and left arrays
+        int left_length = m - l + 1;
+        int right_length = r - m;
+
+        // Allocate memory for both the copy arrays
+        int[] leftArray = new int[left_length];
+        int[] rightArray = new int[right_length];
+
+
+        // Copy the values from the original array that are present on the 
+        // array the is on the left side of the comparison/division
+        for(int i = 0;i < left_length;i++){
+            leftArray[i] = array[l + i];
+        }
+
+
+        // Copy the values from the original array that are present on the 
+        // array the is on the right side of the comparison/division
+        for(int i = 0;i < right_length;i++){
+            rightArray[i] = array[m + 1 + i];
+        }
+
+        // Variable i related to the left side
+        // Variable j related to the right side
+        
+        // This cycle will iterate to every position of the arrays
+        // comparing the values and inserting the smaller value on the 
+        // original array
+        int i,j,k;
+        for(i = 0,j = 0, k = l; k <= r;k++){
+            
+            // Case where there are not elements on the left array
+            // We'll just insert the ones remaining as they already are
+            // sorted
+            if(i == left_length && j < right_length){
+                array[k] = rightArray[j];
+                j++;
+                continue;
+            }
+
+            // Case where there are not elements on the right array
+            // We'll just insert the ones remaining as they already are
+            // sorted
+            if(i < left_length && j == right_length){
+                array[k] = leftArray[i];
+                i++;
+                continue;
+            }
+
+
+            // Main comparison: 
+                // if 
+                    // there are elements on the left AND 
+                        // no more on the right OR the left element is less than the right
+                // 
+            if((i < left_length) && (j >= right_length || leftArray[i] <= rightArray[j])){
+                array[k] = leftArray[i];
+                i++;
+            }else{
+                array[k] = rightArray[j];
+                j++;
+            }
+
+        }
+    }
+    public void printArray(int[] array,int size){
+        if(size > 1){
+            System.out.print("[ " + array[0]);
+            for(int i = 1;i < size;i++){
+                System.out.print(" -> " + array[i]);
+            }
+            System.out.println(" ]");
+            return;
+        }
+        if(size == 1){
+            System.out.println("[ " + array[0] + " ]");
+            return;
+        }
     }
 }
--- poo/ficha2/Main.java
+++ poo/ficha2/Main.java
@@ -5,28 +5,61 @@ public class Main{
     public static void main(String[] args){
         Scanner line = new Scanner(System.in);
         Implementacao f = new Implementacao();
+        switch(line.nextInt()){
+            case 1:
+                switch(line.nextInt()){
+                    case 1:
+                        int[] array1 = readInts(line);
+                        System.out.println("O minimo do array é : " + f.findMin(array1));
+                        break;
+                    case 2:
+                        //Definir o array
+                        array1 = readInts(line);
 
-        int[] array1 = readInts(line);
+                        // Definir os indices do array
+                        int index1 = line.nextInt();
+                        int index2 = line.nextInt();
 
-        System.out.println("O minimo do array é : " + f.findMin(array1));
+                        //Chamar o método para retornar o novo array
+                        int[] novo = f.window(array1,index1,index2);
+                        
+                        // Print do array
+                        f.printArray(novo,novo.length);
+                        break;
+                    case 3:
+                        array1 = readInts(line);
+                        novo = readInts(line);
+                        //int[] comuns = 
+                        f.common(array1,novo);
+                    default:
+                        System.out.println("Não existem mais funções!");
+                        break;
+                }
+                break;
+            default:
+                System.out.println("Função não implementada!");
+                break;
+        }
 
         line.close();
     }   
     private static int[] readInts(Scanner line){
+        Implementacao f = new Implementacao();
         System.out.print("Insira o tamanho do array: ");
         int size = line.nextInt();
         int[] array = new int[size];
         for(int i = 0;i < size;i++){
-            System.out.print("Index: " + i + " :");
+            System.out.print("Index: " + i + " : ");
             int num = line.nextInt();
             array[i] = num;
         }
         System.out.println("Array inserido:");
-        System.out.print("[ " + array[0]);
+        f.printArray(array,size);
+        /*System.out.print("[ " + array[0]);
         for(int i = 1;i < size;i++){
             System.out.print(" -> " + array[i]);
         }
-        System.out.println(" ]");
+        System.out.println(" ]");*/
         return array;
     }
 }
